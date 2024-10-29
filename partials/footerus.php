35c9f931787bb6457f1d29d3071ddd7cce96918a
Yasith Bhanu Sri <88855349+yasithbhanu@users.noreply.github.com>
2024-10-29 16:52:16 +0530

    Add files via upload

---
+++ partials/footerus.php
@@ -0,0 +1,56 @@
+<!-- Start Footer Section -->
+
+<footer>
+    <br /><br />
+            <div class="wrapper1">
+                <div class="container">
+                <div class="sec aboutus">
+                    <h2>About Us</h2>
+                    <p>To offer our ultimate gratitude to this amazing nature by providing the best agricultural 
+                        products to sustain a greener future.</p>
+                    <ul class="sci">
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+                <div class="sec quickLinks">
+                    <h2>Quick Links</h2>
+                    <ul class="qui">
+                        <li><a href="Home.php">Home</a></li>
+                        <li><a href="categories.php">Categories</a></li>
+                        <li><a href="menu.php">Menu</a></li>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="admin/login.php">Login as Admin</a></li>
+                    </ul>
+                </div>
+                <div class="sec cont">
+                    <h2>Contact Info</h2>
+                    <ul class=info>
+                    <li>
+                            <span><i class="fa fa-phone" aria-hidden="true"></i></span>
+                            <p><a href="tel:+94712150200">+94 712 150 200 / </a>
+                                  <a href="tel:+94768470920"> +94 768 470 920</a></p>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                            <p>Aradhana, Dikkapitiya, Welimada.</p>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-envelope-o" aria-hidden="true"></i></i></span>
+                            <p><a href="mailto:aradhanacom16@gmail.com">aradhanacom16@gmail.com</a></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <div class="copy">
+            <p>Copyright <span>&copy</span> 2023 Online Tutorials. All Rights Reserved. </p>
+        </div>
+            </div>
+            
+        <!-- End Footer Section -->
+
+    </body>
+</html>
---
+++ partials/head.php
@@ -0,0 +1,49 @@
+<?php include('config/constants.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aradhana Agro</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="Home.php" title="Logo">
+                    <img src="img/Logo.png" alt="Agro Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>menu.php">Menu</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>user-login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
